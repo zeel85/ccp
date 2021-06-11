1f57f02d3390c9278573ce0b56026d8397066ab3
zeel85 <83920690+zeel85@users.noreply.github.com>
2021-06-11 23:49:14 +0530

    Create 17.5.2021 print the ASCII value of a character.c

---
+++ 17.5.2021 print the ASCII value of a character.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char ch;
+    printf("enter any character\n");
+    scanf("%c",&ch);
+    printf("the ascii value of %c is %d",ch,ch);
+    return 0;
+}
+
+
